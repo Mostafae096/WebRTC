aa4064eff53cb96d158b88b10259b838a47cb37a
Mostafa Ehab <mostafa.ehab@wexcute.com>
2025-06-25 19:33:32 +0800

    test

--- src/app/pages/voice.tsx
+++ src/app/pages/voice.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import * as mediasoupClient from "mediasoup-client";
 
-const socket = io("https://139.162.164.202:3000/");
+const socket = io("https://localhost:3000/");
 
 export default function VoiceRoom() {
   const [inCall, setInCall] = useState(false);
